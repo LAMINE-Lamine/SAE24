0e7e041214001605d53fb2bff9e84c60dca22063
Liam Thomas-Chollier <90798991+LiamTHCH@users.noreply.github.com>
2022-06-22 22:16:25 +0200

    add DB
    Signed-off-by: Liam Thomas-Chollier <90798991+LiamTHCH@users.noreply.github.com>

--- SAE24_GROUPE4-master (1)/SAE24_GROUPE18-master/SAE24/mySAE24/models.py
+++ SAE24_GROUPE4-master (1)/SAE24_GROUPE18-master/SAE24/mySAE24/models.py
@@ -1,25 +1,26 @@
 from django.db import models
 
-class capteur(models.Model):
+class Capteur(models.Model):
+    id = models.CharField(max_length=100,primary_key=True)
     nom = models.CharField(max_length=100)
     piece = models.CharField(max_length=100)
     emplacement = models.CharField(max_length=100)
 
     def __str__(self):
-        chaine = f"{self.nom}"
+        chaine = f"{self.id}"
         return chaine
 
     def dico(self):
         return {"nom": self.nom,"piece": self.piece, "emplacement": self.emplacement}
 
 
-class data(models.Model):
-    data = models.CharField(max_length=200)
-    timestamp = models.CharField(max_length=200)
-    capteur = models.ForeignKey("capteur", on_delete=models.CASCADE,blank=False)
+class Data(models.Model):
+    temp = models.CharField(max_length=200)
+    timestamp = models.DateTimeField(max_length=200)
+    capteur = models.ForeignKey(Capteur, on_delete=models.CASCADE,blank=False)
 
     def __str__(self):
-        chaine = self.data + "," + self.timestamp + "," + capteur.objects.get(id=self.capteur).piece + " | " + capteur.objects.get(id=self.capteur).nom
+        chaine = self.temp + "," + str(self.timestamp) + "," + self.capteur.piece + " | " + self.capteur.nom
         return chaine
 
     def dico(self):
